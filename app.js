06bd601b23248865780919adb057e560c1003eef
Ugurhamzaoglu67 <craxx67@gmail.com>
2021-05-09 16:19:58 +0300

    Created routes & dbConnection & views/index.ejs

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express')
+const { connect } = require('mongodb')
 const app = express()
 require('dotenv').config()
 const PORT = process.env.PORT
 const path = require('path')
+const connectDB = require('./utility/database')
+const  employeeRoutes = require('./routes/employeeRoutes')
 
+// MONGO DB connection
+connectDB()
 
 app.set("views", path.join(__dirname,"views"))
 app.set("view engine","ejs")
 
-
 app.use(express.static('public'))
+
+
+app.use('/',employeeRoutes)
 
 
 
